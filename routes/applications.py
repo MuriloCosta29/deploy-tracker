ec69d731fbc0f65ecd61d07237d91a11056202cd
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-03-18 20:04:56 -0300

    feat: create CRUD routes for applications

---
+++ routes/applications.py
@@ -0,0 +1,74 @@
+# NOTE: All endpoints related to the /applications resource
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from models import Application
+from database import get_session
+from schemas import ApplicationItem
+
+# Route files create routers -- Smaller groups of routes that plug into the
+# main.app
+# GET /applications - List all
+# GET /applications/{id} - get one
+# POST /applications - create
+# PATCH /applications/{id} - edit
+# DELETE /applications/{id} - delete
+# -------------------------------------------------------------------------------
+router = APIRouter()
+
+# ------------------------------------------------------------------------------------------------
+
+
+@router.get("/applications")
+def list_applications(db: Session = Depends(get_session)):
+    applications = db.query(Application).all()
+    return applications
+
+
+# ------------------------------------------------------------------------------------------------
+
+
+@router.get("/applications/{id}")
+def one_application(id: int, db: Session = Depends(get_session)):
+    one_application = db.query(Application).filter(Application.id == id).first()
+    return one_application
+
+
+# ------------------------------------------------------------------------------------------------
+
+
+@router.post("/applications")
+def create_application(
+    application_data: ApplicationItem, db: Session = Depends(get_session)
+):
+    new_app = Application(name=application_data.name, url=application_data.url)
+    db.add(new_app)
+    db.commit()
+    db.refresh(new_app)
+    return new_app
+
+
+# ------------------------------------------------------------------------------------------------
+
+
+@router.delete("/applications/{id}")
+def delete_application(id: int, db: Session = Depends(get_session)):
+    del_app = db.query(Application).filter(Application.id == id).first()
+    db.delete(del_app)
+    db.commit()
+    return del_app
+
+
+# ------------------------------------------------------------------------------------------------
+
+
+@router.patch("/applications/{id}")
+def change_app(
+    id: int, application_data: ApplicationItem, db: Session = Depends(get_session)
+):
+    changing_app = db.query(Application).filter(Application.id == id).first()
+    changing_app.name = application_data.name
+    changing_app.url = application_data.url
+    db.commit()
+    db.refresh(changing_app)
+    return changing_app
