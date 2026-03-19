792dccb5ac36f33e7c0cc9eadbae1a8c889a7109
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-03-19 19:57:32 -0300

    feat: teste and validate full API with PostgreSQL

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 # NOTE: Entry point of application. Creates the FastAPI and connects all routers.
+# NOTE: `as` set a nickname for import
+# -------------------------------------------------------------------------------
 
 from fastapi import FastAPI
 from routes.applications import router as applications_router
 from routes.deploys import router as deploys_router
-
-# NOTE: `as` set a nickname for import
+from database import Base, engine
 
 # -------------------------------------------------------------------------------
+
 app = FastAPI()
 app.include_router(applications_router)
 app.include_router(deploys_router)
+
+# -------------------------------------------------------------------------------
+
+Base.metadata.create_all(
+    bind=engine
+)  # Creates the table in the database (not file! File is the SQLite. Now is database, only.)
+
 # -------------------------------------------------------------------------------
 
 
