8bcb872434e663b6a16a0a9ad36c0a0a8fb76466
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-03-27 18:56:13 -0300

    feat: add Redis cache helper functions closes #8

--- cache.py
+++ cache.py
@@ -4,24 +4,37 @@
 import redis
 import json
 
-json = json
+from redis.typing import ExpiryT
+
 # -------------------------------------------------
 
 r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 # decode_responses means -> Redis returns strings instead of bytes, which is easier to work with.
-
+# -------------------------------------------------
+# json.dumps() -> Convert Python to string.
+# json.loads() -> Convert JSON string back to python
 
 # -------------------------------------------------
 # Gets data from Redis by key, important: Returns Python object or none if not found.
+# IMPORTANT: I have to convert for strings two(2) reasons
+# 1. Redis can only store strings.
+# 2. The endpoints returns Python objects (lists, dicts).
+
+
 def get_cache(key):
-    r.get(key)
+    value = r.get(key)
+    if value is not None:
+        new_value = json.loads(value)
+        return new_value
+
+    return None
 
 
 # -------------------------------------------------
 # Converts data to JSON and stores in Redis
 def set_cache(key, data):
-    data = get_cache(key)
-    json.dumps(data)
+    value = json.dumps(data)
+    r.set(key, value, ex=300)
 
 
 # -------------------------------------------------
