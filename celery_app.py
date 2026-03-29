17176b5a7cf744c9f2f0d75af56ca647c76e0345
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-03-29 13:49:18 -0300

    feat: configure Celery with Redis as broker closes #13

---
+++ celery_app.py
@@ -0,0 +1,9 @@
+from celery import Celery
+# -------------------------------------------------
+
+# the name can't have uppercase or space.
+celery_app = Celery(
+    "deploy_tracker",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0",
+)
---
+++ tasks.py
@@ -0,0 +1,7 @@
+from celery_app import celery_app
+# -------------------------------------------------
+
+
+@celery_app.task
+def hello():
+    return "hello, world!"
